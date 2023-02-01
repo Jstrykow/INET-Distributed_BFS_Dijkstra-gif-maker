fe162ee086755311581294f4b8defd44be17bb9e
jakub@strykowski.eu <j.strykowski@stud.elka.pw.edu.pl>
2023-02-01 17:41:41 +0100

    add node coloring

--- draw.py
+++ draw.py
@@ -13,6 +13,10 @@ g = nx.Graph()
 nodes = [1, 2, 3, 4, 5]
 edges = [(1, 2), (2, 4), (3, 5), (2,3), (4,5), (2,5)]
 
+# add node coloring
+# add algorithms, which would color nodes
+# delete or replace img after changing color; maybe a function
+
 # names of files
 filenames = []
 directory = "img/"
