c60185bb66c74282e030f776648257429d345ec0
jakub@strykowski.eu <j.strykowski@stud.elka.pw.edu.pl>
2023-02-11 13:16:00 +0100

    sth

--- dijkstra.py
+++ dijkstra.py
@@ -22,7 +22,7 @@ def dijkstra(G: nx.Graph(), root_id):
     current_node = root_id
 
     # def
-    utils.draw_graph(G, blue_nodes=unknow_nodes, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS,red_edges=red_edges, orange_nodes=orange_nodes)
+    utils.draw_graph(G, blue_nodes=edges, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS,red_edges=red_edges, orange_nodes=orange_nodes)
 
 
     # broadcast p
@@ -30,7 +30,7 @@ def dijkstra(G: nx.Graph(), root_id):
     orange_edges= [n for n in G.edges(root_id)]
     nodes_in_phase = {phase: orange_nodes.copy()}
     edges_in_phase = {phase: orange_edges.copy()}
-    utils.draw_graph(G, blue_nodes=unknow_nodes, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS,red_edges=red_edges, orange_nodes=orange_nodes)
+    utils.draw_graph(G, blue_nodes=edges, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS,red_edges=red_edges, orange_nodes=orange_nodes)
 
     # ack first edges all add to BFS
     BFS = edges_in_phase[phase].copy()
@@ -38,12 +38,15 @@ def dijkstra(G: nx.Graph(), root_id):
     know_edges+= orange_edges
     for e in know_edges:
        remove_edge(e, unknow_edges)
-    orange_edges= []
-    orange_nodes= []
 
-    utils.draw_graph(G, blue_nodes=unknow_nodes, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS,red_edges=red_edges, orange_nodes=orange_nodes)
+    #utils.draw_graph(G, blue_nodes=unknow_nodes, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS,red_edges=red_edges, orange_nodes=orange_nodes)
 
-    while len(unknow_edges) > 0:
+    while len(orange_edges) != 0:
+        orange_edges = []
+        orange_nodes = []
+        red_edges = []
+        utils.draw_graph(G, blue_nodes=edges, green_nodes=know_nodes, orange_edges=[], green_edges=BFS, red_edges=red_edges, orange_nodes=[])
+       
         for current_node in nodes_in_phase[phase]:
             for e in G.edges(current_node):
                 if e not in know_edges and (e[1], e[0]) not in know_edges:
@@ -57,7 +60,7 @@ def dijkstra(G: nx.Graph(), root_id):
         edges_in_phase = {phase+1: orange_edges}
         
         #draw sending ACK
-        utils.draw_graph(G, blue_nodes=unknow_nodes, green_nodes=know_nodes, orange_edges=orange_edges,green_edges=BFS, red_edges=red_edges, orange_nodes=orange_nodes)
+        utils.draw_graph(G, blue_nodes=edges, green_nodes=know_nodes, orange_edges=orange_edges,green_edges=BFS, red_edges=red_edges, orange_nodes=orange_nodes)
         # draw BFS
         for e in orange_edges:
             if e[0] and e[1] in know_nodes:
@@ -69,14 +72,8 @@ def dijkstra(G: nx.Graph(), root_id):
                 know_nodes.append(e[1])
                 know_nodes = list(dict.fromkeys(know_nodes))
                 remove_edge(e, unknow_edges)
-        utils.draw_graph(G, blue_nodes=unknow_nodes, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS, red_edges=red_edges, orange_nodes=orange_nodes)
+        utils.draw_graph(G, blue_nodes=edges, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS, red_edges=red_edges, orange_nodes=orange_nodes)
         
-        know_edges+= orange_edges
-        orange_edges = []
-        orange_nodes = []
-        red_edges = []
-        utils.draw_graph(G, blue_nodes=unknow_nodes, green_nodes=know_nodes, orange_edges=orange_edges, green_edges=BFS, red_edges=red_edges, orange_nodes=orange_nodes)
-        red_edges= []
         phase+= 1
         print(nodes_in_phase)
         print(edges_in_phase)
@@ -89,3 +86,8 @@ def remove_edge(edge, edges):
             edges.remove((edge[1], edge[0]))
     except:
         pass
+
+def sort_tuple_list(tuple_list):
+    sorted_tuple_list = []
+    for n in tuple_list:
+        sorted_tuple_list.append(sorted(n))
